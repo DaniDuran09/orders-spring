7faa2903c0a256d4df3a11cae93d5cf90379d5fb
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-04-19 22:53:29 -0600

    create: project structure

---
+++ src/test/java/com/daniel/ordersspring/OrdersSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.ordersspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
